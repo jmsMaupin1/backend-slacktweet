e3fd7784d6b2cf5dfc7451255d66c84750e40086
James Maupin <jmsMaupinRC@gmail.com>
2020-01-10 21:51:10 -0500

    Refactor to slackclient v2 and add help and ping commands

--- slack_client.py
+++ slack_client.py
@@ -2,82 +2,120 @@
 """A standalone Slack client implementation
 see https://slack.dev/python-slackclient/
 """
-import time
 import os
 import re
 import logging
 from datetime import datetime as dt
 
 from dotenv import load_dotenv
-from slackclient import SlackClient
+from slack import RTMClient, WebClient
 
 load_dotenv()
 logger = logging.getLogger(__name__)
 
 
 class SlackBotClient:
+    """"""
 
-    def __init__(self, token, delay=1, logger=None):
-        self.client = SlackClient(token)
-        self.id = None
-        self.logger = logger
-        self.exit_flag = False
-        self.delay = delay
-        self.mention_regex = r"^<@(|[WU].+?)>(.*)"
+    def __init__(self, token):
+        self.rtm_client = RTMClient(token=token, run_async=True)
+        self.id = WebClient(token).api_call('auth.test')['user_id']
+        self.start_time = None
+        RTMClient.run_on(event="message")(self.read_message)
+        self.future = self.rtm_client.start()
+        self.commands = {
+            "help": {
+                "help": "Shows this helpful command reference",
+                "func": lambda cmd, chan, wc: self.send_help_message(chan, wc)
+            },
+            "ping": {
+                "help": "Show uptime of the bot",
+                "func": lambda cmd, chan, wc: self.send_ping_message(chan, wc)
+            }
+        }
+        self.commands_old = [
+            {
+                "command": "help",
+                "help": "Shows this helpful command reference"
+            },
+            {
+                "command": "ping",
+                "help": "Show uptime of the bot"
+            },
+            {
+                "command": "exit",
+                "help": "Shutdown the bot"
+            },
+            {
+                "command": "quit",
+                "help": "Shutdown the bot"
+            },
+            {
+                "command": "list",
+                "help": "List current Twitter filters and counts"
+            },
+            {
+                "command": "add",
+                "help": "Add some twitter keyword filter"
+            },
+            {
+                "command": "del",
+                "help": "Remove some twitter keyword filter"
+            },
+            {
+                "command": "clear",
+                "help": "Remove all twitter keyword filters"
+            },
+            {
+                "command": "raise",
+                "help": "Manually raise an exception"
+            }
+        ]
 
     def __enter__(self):
-        if self.client.rtm_connect(with_team_state=False):
-            self.id = self.client.api_call('auth.test')['user_id']
+        return self
 
-            self.logger.debug('Bot connected to slack, id: {}'.format(self.id))
+    def __exit__(self, a, b, c):
+        logger.debug('exiting')
 
-            while self.client.server.connected:
-                command, channel = self.parse_bot_commands(
-                    self.client.rtm_read())
-                if command:
-                    self.handle_command(command, channel)
+    def connect_to_stream(self):
+        loop = self.future.get_loop()
+        self.start_time = dt.now()
+        loop.run_until_complete(self.future)
 
-                time.sleep(self.delay)
+    def read_message(self, **kwargs):
+        data = kwargs['data']
+        web_client = kwargs['web_client']
+        message = data['text']
+        channel = data['channel']
 
-    def __exit__(self):
-        self.logger.debug('Bot disconnected')
+        matches = re.search(r"^<@(|[WU].+?)>(.*)", message)
 
-    def parse_bot_commands(self, slack_events):
-        for event in slack_events:
-            if ('type' in event
-                    and event['type'] == 'message'
-                    and 'subtype' not in event):
+        if matches and matches.group(1) == self.id:
+            self.handle_command(matches.group(2).strip(), channel, web_client)
 
-                user_id, message = self.parse_direct_mention(event['text'])
-                self.logger.info(
-                    'parse_direct_mention():\n'
-                    'user_id: {}\n'
-                    'message: {}\n'
-                    .format(user_id, message)
-                )
-                if user_id == self.id:
-                    return message, event['channel']
+    def handle_command(self, command, channel, web_client):
+        if command in self.commands:
+            self.commands[command]['func'](command, channel, web_client)
 
-        return None, None
+    def send_help_message(self, channel, web_client):
+        help_s = '```\n'
+        for cmd in self.commands:
+            print(cmd)
+            help_s += f"{cmd}: {self.commands[cmd]['help']}\n"
+        help_s += '```'
 
-    def handle_command(self, command, channel):
-        if command.startswith('echo'):
-            self.logger.info(
-                'handle_command(command, channel)\n'
-                'command: {}\n'
-                'channel: {}\n'
-                .format(command, channel)
-            )
-            self.client.rtm_send_message(
-                channel,
-                command[5:]
-            )
+        web_client.chat_postMessage(
+            channel=channel,
+            text=help_s
+        )
 
-    def parse_direct_mention(self, message_text):
-        matches = re.search(self.mention_regex, message_text)
-        if matches:
-            return (matches.group(1), matches.group(2).strip())
-        return (None, None)
+    def send_ping_message(self, channel, web_client):
+        up_time = dt.now() - self.start_time
+        web_client.chat_postMessage(
+            channel=channel,
+            text=f"Bot has been active for: {up_time}"
+        )
 
 
 def main():
@@ -98,8 +136,8 @@ def main():
         .format(__file__, app_start_time.isoformat())
     )
 
-    with SlackBotClient(os.environ['BOT_TOKEN'], 1, logger):
-        pass
+    with SlackBotClient(os.environ['SLACK_BOT_TOKEN']) as client:
+        client.connect_to_stream()
 
     uptime = dt.now() - app_start_time
     logging.info(
